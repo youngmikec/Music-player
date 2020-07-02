ce7187ee47fd9937500b8d58579839369f395dac
ozor michael chisom <michaelozor15@gmail.com>
2020-07-02 09:13:03 +0100

    change the look of the player

--- js/index.js
+++ js/index.js
@@ -1,15 +1,57 @@
 const overlay = document.querySelector('#overlay');
 const root = document.querySelector('#root');
 const musicList = [
-    'Kizz-Daniel-Pak-N-Go.mp3',
-    '15-BROWN-SKIN-GIRL-feat.-Blue-Ivy-C-Wizkid.mp3',
-    '50_CENT_-_P.I.M.P._Pesni-Tut.mp3',
-    'Olamide_Motigbana_9jaflaver.com_.mp3',
-    'Rema - Iron Man (Pitakwa360.com Video_1559504979.mp3',
-    'Simi-–-Lovin.mp3',
-    'Skales_-_Booty_Language_Remix_Ft__Sarkodie.mp3'
+    {
+        id: 1,
+        name: 'Kizz-Daniel-Pak-N-Go.mp3',
+        img: '../uploads/falz the bad guy.jpg',
+        isFavorite: false,
+        artist: 'kizz Daniel'
+    },
+    {
+        id: 2,
+        name: '15-BROWN-SKIN-GIRL-feat.-Blue-Ivy-C-Wizkid.mp3',
+        img: '../uploads/falz the bad guy.jpg',
+        isFavorite: false,
+        artist: 'Beyonce fit wizkid'
+    },
+    {
+        id: 3,
+        name: '50_CENT_-_P.I.M.P._Pesni-Tut.mp3',
+        img: '../uploads/falz the bad guy.jpg',
+        isFavorite: false,
+        artist: '50_CENT'
+    },
+    {
+        id: 4,
+        name: 'Olamide_Motigbana_9jaflaver.com_.mp3',
+        img: '../uploads/falz the bad guy.jpg',
+        isFavorite: false,
+        artist: 'Olamide'
+    },
+    {
+        id: 5,
+        name: 'Rema - Iron Man (Pitakwa360.com Video_1559504979.mp3',
+        img: '../uploads/msc-image.jpg',
+        isFavorite: false,
+        artist: 'Rema'
+    },
+    {
+        id: 6,
+        name: 'Simi-–-Lovin.mp3',
+        img: '../uploads/falz the bad guy.jpg',
+        isFavorite: false,
+        artist: 'simi'
+    },
+    {
+        id: 7,
+        name: 'Skales_-_Booty_Language_Remix_Ft__Sarkodie.mp3',
+        img: '../uploads/msc-image.jpg',
+        isFavorite: false,
+        artist: 'skales'
+    } 
 ];
-console.log(overlay); //eslint-disable-line no-console
+ //eslint-disable-line no-console
 const loadScreen = () =>{
     overlay.classList.remove('d-visible');
     overlay.classList.add('d-none');
@@ -21,22 +63,36 @@ const loadScreen = () =>{
 //     overlay.classList.add('d-none');
 // }
 //unLoadScreen();
+// display the intro screen for 3seconds
 setTimeout(()=>{
     loadScreen();
 }, 3000);
 
+//variable declarations
 const buttons = document.querySelectorAll('a');
 const audio = document.querySelector('audio');
 const loopBtn = document.querySelector('#loop');
 const shuffleBtn = document.querySelector('#shuffle');
+const progressBar = document.querySelector('#progress-bar');
+const progress = document.querySelector('#progress');
+const thumbnail = document.querySelector('#thumbnail');
+
+const mscName = document.querySelector('#msc-name');
+const mscArtist = document.querySelector('#msc-artist');
+
+
+
 let loop = false;
 let shuffle = false;
+let mouseDown = false;
 let c = 0;
-console.log(buttons, audio);
+let angle = 0;
+
 
 //=============== functions ================
+// Repeats a single song when toggled on
 const toggleLoop = () =>{
-    //changes the toggles the loop true or false
+    //changes the  loop to true or false
     loop = !loop;
     if(loop){
         loopBtn.style.border = '1px solid red';
@@ -44,16 +100,25 @@ const toggleLoop = () =>{
         loopBtn.style.border = 'none';
     }
 }
+const populateMscDetails = (c) =>{
+    let current = musicList[c];
+    mscArtist.textContent = current.artist;
+    mscName.textContent = current.name;
+}
 
+const background = () =>{
+    thumbnail.style.background = `url('${musicList[c].img}')`;
+    thumbnail.parentNode.style.background = `url('${musicList[c].img}')`;
+}
 const toggleShuffle = () =>{
-    //changes the toggles the loop true or false
+    //changes the shuffle to true or false
     shuffle = !shuffle;
     if(shuffle){
         shuffleBtn.style.border = '1px solid red';
-        console.log('shuffle is on');
+        
     }else{
         shuffleBtn.style.border = 'none';
-        console.log('shuffle is off');
+        
     }
 }
 const timeupdate = () =>{
@@ -61,7 +126,9 @@ const timeupdate = () =>{
         if(audio.ended == true){
             audio.currentTime = 0;
             audio.play();
-            buttons[1].innerText = 'pause';
+            // change the icon here
+            buttons[1].childNodes.setAttribute('class', 'fa fa-pause');
+            populateMscDetails(c);
         }
     }else if(shuffle){
         if(audio.ended == true){
@@ -70,59 +137,94 @@ const timeupdate = () =>{
     }else{
         if(audio.ended){
             nextMusic();
-            console.log('music ended and next is playing');
         }else{
-            console.log('waiting for the current music to end');
+            console.log('waiting for the current music to end');// for catching error
         }
     }
     if(audio.paused == false){
-        buttons[1].innerText = 'pause';
+        //change the icon here
+        buttons[1].firstElementChild.setAttribute('class', 'fa fa-pause');
+        populateMscDetails(c);
     }
 
 }
+// updates the music progress bar with the current time  
+const updateBAr = () =>{
+    let percent = (audio.currentTime / audio.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+    
+}
+const scrub = (e) =>{
+    const scrubTime = (e.offsetX / progressBar.parentNode.offsetWidth) * audio.duration;
+    audio.currentTime = scrubTime;
+    
+}
+const toggleMouseDown = () =>{
+    return mouseDown = !mouseDown;
+}
+const mouseDrag = (e) =>{
+    // let isDown = toggleMouseDown();
+    // if(isDown){
+    //     console.log(e);
+    // };
+}
+// rotate thumbnail
+const rotateThumbanil = () =>{
+    thumbnail.style.transform = `rotate(${angle += 3}deg)`;
+    if(angle >= 360) angle = 0;
+}
+// shuffle the sequence of the songs in the playlist when toggled on
 const shuffleMusic = () =>{
     let randNumber = Math.floor(Math.random()*musicList.length);
     audio.setAttribute('src',
-    `../uploads/${musicList[randNumber]}`);
+    `../uploads/${musicList[randNumber].name}`);
     audio.play();
-    console.log(randNumber);
+    background();
+    // console.log(randNumber);
 }
+
+// plays the next music in the list
 const nextMusic = () =>{
     c++;
     if(c == musicList.length){
         c = 0;
     }
-    audio.setAttribute('src', `../uploads/${musicList[c]}`);
+    audio.setAttribute('src', `../uploads/${musicList[c].name}`);
     audio.play();
+    populateMscDetails(c);
+    background();
 }
 const previousMusic = () =>{
     c--;
     if(c < 0){
         c = musicList.length - 1;
     }
-    audio.setAttribute('src', `../uploads/${musicList[c]}`);
+    audio.setAttribute('src', `../uploads/${musicList[c].name}`);
     audio.play();
+    populateMscDetails(c);
+    background();
 }
 
 
 // adding of events
 Array.from(buttons).forEach(btn =>{
     btn.addEventListener('click', ()=>{
+        let id = btn.getAttribute('id');
         console.log(btn.innerText);
-        if(btn.innerText == 'play'){
+        if(id == 'play'){
             audio.play();
-            btn.innerText = 'pause';
-
+            btn.setAttribute('id', 'pause');
+            btn.firstElementChild.setAttribute('class', 'fa fa-pause');
+            background();
         }
-        else if(btn.innerText == 'pause'){
+        else if(id == 'pause'){
             audio.pause();
-            btn.innerText = 'play';
-            console.log(audio.ended);
-         }else if(btn.textContent === 'previous'){
-            //alert('previous music is playing');
+            btn.setAttribute('id', 'play');
+            btn.firstElementChild.setAttribute('class', 'fa fa-play');
+            background();
+         }else if(id === 'prev'){
             previousMusic();
-        }else if(btn.textContent === 'next'){
-            //alert('next music is playing');
+        }else if(id === 'next'){
             nextMusic();
          }else{
             console.log('click a valid button');
@@ -132,6 +234,8 @@ Array.from(buttons).forEach(btn =>{
 
 audio.addEventListener('timeupdate', ()=>{
     timeupdate();
+    updateBAr();
+    rotateThumbanil();
 });
 
 
@@ -141,3 +245,8 @@ loopBtn.addEventListener('click', ()=>{
 shuffleBtn.addEventListener('click', ()=>{
     toggleShuffle();
 });
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousedown', ()=> mouseDown = true);
+progress.addEventListener('mouseup', ()=> mouseDown = false);
+progress.addEventListener('mousemove', (e)=> (mouseDown) && scrub(e));
